b62d4784971efdaa98243376de6c3c923a7991ae
Keith Manaloto <keithliamm@gmail.com>
2022-04-22 11:05:03 +0800

    Make Topic subcomponent

--- src/components/landing/PillarsSection/PillarsSection.tsx
+++ src/components/landing/PillarsSection/PillarsSection.tsx
@@ -4,12 +4,12 @@ import styled from '@emotion/styled'
 import { ClassNames, css } from '@emotion/react'
 import Sticky from 'react-stickynode'
 import { SwitchTransition, CSSTransition } from 'react-transition-group'
+import Topic from './Topic'
 import PhotoCredit from '../../PhotoCredit'
-import WingText from '../../WingText'
 import { useSectionVisibilityObserver, useSubsectionIndexObserver } from '../../../hooks'
 import { registerScrollResizeEventListeners } from '../../../helpers'
 import { topics } from '../../../data/pillars'
-import { COLORS, FONT_FAMILY, FONT_WEIGHT, MEDIAQUERY, PADDING } from '../../../constants'
+import { COLORS, FONT_FAMILY, MEDIAQUERY, PADDING } from '../../../constants'
 
 const PILLARS_SECTION_ID = 'pillars'
 
@@ -119,57 +119,6 @@ const photoCreditShownStyles = css`
   }
 `
 
-const Topic = styled.div`
-  font-size: 49px;
-  font-family: ${FONT_FAMILY.HEADING};
-  letter-spacing: 0.06em;
-  font-weight: ${FONT_WEIGHT.NORMAL};
-  z-index: 1;
-
-  ${MEDIAQUERY.LARGE} {
-    font-size: 47px;
-  }
-
-  ${MEDIAQUERY.SMALL} {
-    font-size: 42px;
-  }
-
-  ${MEDIAQUERY.TABLET} {
-    font-size: 38px;
-  }
-
-  ${MEDIAQUERY.MOBILE} {
-    font-size: 34px;
-  }
-`
-
-const Highlight = styled.span`
-  color: ${COLORS.BA_GOLD};
-`
-
-const wingTextTransitionStyles = {
-  enter: `
-    transform: translateY(75%);
-    opacity: 0;
-    transition: all 0.8s cubic-bezier(0.77, 0, 0.18, 1);
-  `,
-  enterExitActive: `
-    transform: translateY(0);
-    opacity: 1;
-    transition: all 0.8s cubic-bezier(0.77, 0, 0.18, 1);
-  `,
-  exit: `
-    transform: translateY(-75%);
-    opacity: 0;
-    transition: all 0.8s cubic-bezier(0.77, 0, 0.18, 1);
-  `,
-}
-const wingTextStyles = css`
-  display: inline-block;
-  z-index: 100;
-  color: white;
-`
-
 const descriptionTransitionStyles = {
   enterExitExitActive: `
     transition: opacity 1s cubic-bezier(0.87, 0.2, 0.61, 0.99);
@@ -269,23 +218,7 @@ const PillarsSection = () => {
                   name="Brandon Evangelista"
                   link="https://bit.ly/bayung-angeles-portfolio-brandon"
                 />
-                <Topic>
-                  <Highlight>Our </Highlight>
-                  <SwitchTransition mode="out-in">
-                    <CSSTransition
-                      key={title}
-                      classNames={{
-                        enter: generateCss(wingTextTransitionStyles.enter),
-                        enterActive: generateCss(wingTextTransitionStyles.enterExitActive),
-                        exit: generateCss(wingTextTransitionStyles.exit),
-                        exitActive: generateCss(wingTextTransitionStyles.enterExitActive),
-                      }}
-                      timeout={500}
-                    >
-                      <WingText css={wingTextStyles} text={title} wingPosition="end" />
-                    </CSSTransition>
-                  </SwitchTransition>
-                </Topic>
+                <Topic topic={title} />
                 <SwitchTransition mode="out-in">
                   <CSSTransition
                     key={description}
