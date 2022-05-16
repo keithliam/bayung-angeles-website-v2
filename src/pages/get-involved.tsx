e4b13b92ad5b0c8cee44937ed6c4110608bd304b
Keith Manaloto <keithliamm@gmail.com>
2022-05-17 01:47:23 +0800

    Add a Get Involved page

---
+++ src/pages/get-involved.tsx
@@ -0,0 +1,7 @@
+import Layout from '../layout/Layout'
+import '../fonts/fonts.css'
+import '@fontsource/public-sans/variable.css'
+
+const GetInvolvedPage = () => <Layout showNavbarLogo />
+
+export default GetInvolvedPage
