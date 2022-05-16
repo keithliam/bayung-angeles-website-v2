b36fa24c6cb054459b59b17d8d00bea96fc1d618
Keith Manaloto <keithliamm@gmail.com>
2022-05-17 01:56:14 +0800

    Add a blank container

--- src/pages/get-involved.tsx
+++ src/pages/get-involved.tsx
@@ -1,7 +1,18 @@
+import styled from '@emotion/styled'
 import Layout from '../layout/Layout'
 import '../fonts/fonts.css'
 import '@fontsource/public-sans/variable.css'
 
-const GetInvolvedPage = () => <Layout showNavbarLogo />
+const Container = styled.div`
+  height: calc(100vh - 67px);
+  width: 100vw;
+  background-color: white;
+`
+
+const GetInvolvedPage = () => (
+  <Layout showNavbarLogo>
+    <Container />
+  </Layout>
+)
 
 export default GetInvolvedPage
