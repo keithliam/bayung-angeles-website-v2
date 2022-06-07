8f4ea466765d3eb718a9891cb8486c53cdaf6a30
Keith Manaloto <keithliamm@gmail.com>
2022-06-07 23:09:58 +0800

    Add StickersSubsection component

---
+++ src/components/getInvolved/StickersSubsection/StickersSubsection.tsx
@@ -0,0 +1,19 @@
+import Subsection from '../Subsection/Subsection'
+import DownloadsPreview from './DownloadsPreview/DownloadsPreview'
+import { allStickersLink, stickers } from '../../../data/downloads'
+
+const Header = () => 'Sticker Designs'
+
+const Content = () => <DownloadsPreview previews={stickers} />
+
+const StickersSubsection = () => (
+  <Subsection
+    headerComponent={Header}
+    contentComponent={Content}
+    link={allStickersLink}
+    linkButtonText="Download All"
+    smallPreview
+  />
+)
+
+export default StickersSubsection
